1418fc2c562b7ada2e0bed466e39d7ad2d816650
Pravijith-j-p <148673118+Pravijith-j-p@users.noreply.github.com>
2024-09-22 08:23:19 +0530

    url

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const PORT =process.env.PORT || 5000
 const app =express();
 
 app.use(cors({
-    origin :['http://localhost:3000', "http://localhost:3001"],
+    origin :['http://localhost:3000', "http://localhost:3001","https://cloudbased-task-manager.onrender.com"],
     methods :["GET", "POST", "PUT", "DELETE"],
     credentials :true,
 })
