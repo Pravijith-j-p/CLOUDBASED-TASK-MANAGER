d12fb4e19216f2b647cb4a4bff425dc2d9e9c9e6
Pravijith-j-p <148673118+Pravijith-j-p@users.noreply.github.com>
2024-09-23 19:50:53 +0530

    commits

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URI = "http://localhost:8800/api";
-const API_URI = import.meta.env.VITE_APP_BASE_URL || "https://cloudbased-task-manager-1.onrender.com";
+const API_URI = import.meta.env.VITE_APP_BASE_URL || "https://cloudbased-task-manager.onrender.com";
 
 const baseQuery = fetchBaseQuery({ baseUrl: `${API_URI}/api` }); 
 
